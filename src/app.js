cd879c4d995eb81f791fa1dea09561c6a54b7734
ArnauOliveras <77742762+ArnauOliveras@users.noreply.github.com>
2024-11-27 20:04:44 +0100

    first

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let nums = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let stiks = ["♦", "♥", "♠", "♣"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("changeCardButton").addEventListener("click", e => {
+    getRandomCard();
+  });
+
+  setInterval(getRandomCard, 10000);
+  getRandomCard();
 };
+
+function getRandomCard() {
+  document.getElementById("num").textContent = getRandomElement(nums);
+
+  let stickIcon = getRandomElement(stiks);
+  let stickClass;
+  switch (stickIcon) {
+    case "♦":
+      stickClass = "diamond";
+      break;
+    case "♥":
+      stickClass = "heart";
+      break;
+    case "♠":
+      stickClass = "spade";
+      break;
+    case "♣":
+      stickClass = "club";
+      break;
+  }
+
+  document.getElementById("stick_1").className = "";
+  document.getElementById("stick_1").classList.add(stickClass);
+  document.getElementById("stick_2").className = "";
+  document.getElementById("stick_2").classList.add(stickClass);
+  document.getElementById("stick_1").textContent = stickIcon;
+  document.getElementById("stick_2").textContent = stickIcon;
+}
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
